6a353f464e7ba676f774a9f8ad7dcaeb591e5464
MikuraAngels <i-release-my-life@live.fr>
2013-11-29 13:15:57 +0100

    Ajout de la classe Visiteur

---
+++ src/solene/Visiteur.java
@@ -0,0 +1,63 @@
+package solene;
+
+public class Visiteur {
+	
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String codePostal;
+	private String ville;
+
+	
+	public Visiteur(String sonNom, String sonPrenom, String sonAdresse, String sonCodePostal, String saVille){
+		this.nom=sonNom;
+		this.prenom=sonPrenom;
+		this.adresse = sonAdresse;
+		this.codePostal = sonCodePostal;
+		this.ville = saVille;
+		
+		
+	}
+
+	
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+}
